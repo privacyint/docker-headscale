65c6820df1593d47dd8d23b5d8cf1870a61cbeb6
Ed Geraghty <ed@geraghty.family>
2025-06-10 22:25:44 +0100

    Make boolean logic explicit

--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -373,9 +373,9 @@ run() {
 	check_config_files
 
 	if ! $abort_config ; then
-		log_info "Starting Caddy using our environment variables. HTTPS is $([ "$cleartext_only" ] && echo "disabled" || echo "enabled")."
+		log_info "Starting Caddy using our environment variables. HTTPS is $([ "$cleartext_only" = true ] && echo "disabled" || echo "enabled")."
 
-		if $cleartext_only ; then
+		if [ "$cleartext_only" = true ] ; then
 			caddy start --config "$caddyfile_cleartext" || log_error "Failed to start Caddy with cleartext config"
 		else
 			caddy start --config "$caddyfile_https" || log_error "Failed to start Caddy with HTTPS config"
@@ -383,7 +383,7 @@ run() {
 
 		# Make sure Caddy started successfully before starting headscale
         if ! $abort_config ; then
-			if ! $litestream_disabled ; then
+			if [ "$litestream_disabled" = false ] ; then
 				log_info "Attempt to restore previous Headscale database if there's a replica"
 				litestream restore -if-db-not-exists -if-replica-exists /data/headscale.sqlite3 ||
 					log_warn "No replica found, or unable to restore database."
