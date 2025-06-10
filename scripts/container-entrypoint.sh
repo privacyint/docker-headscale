692ac0cc45103f21cd9e3cc33f9bc0925e5e0f11
Ed Geraghty <ed@geraghty.family>
2025-06-10 22:06:18 +0100

    Make sure our noise key has appropriate perms

--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -258,12 +258,14 @@ reuse_or_create_noise_private_key() {
 
 	if [ -f "$key_path" ]; then
 		log_info "Using existing private Noise key on disk."
+		chmod 600 "$key_path"
 		return
 	fi
 
 	if env_var_is_populated "HEADSCALE_NOISE_PRIVATE_KEY"; then
 		log_info "Using provided private Noise key from environment variable."
 		echo -n "$HEADSCALE_NOISE_PRIVATE_KEY" > "$key_path"
+        chmod 600 "$key_path"
 	else
 		log_info "Generating a new private Noise key."
 	fi
