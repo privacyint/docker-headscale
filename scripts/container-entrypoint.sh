d0e726a65b6554aeda397b62f957c2cd777f3050
Ed Geraghty <ed@geraghty.family>
2025-08-25 18:25:37 +0100

    Move file operations out to a helper script

--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -10,6 +10,7 @@ declare helper_scripts=(
 	"defaults.sh"
 	"logging.sh"
 	"variables-check.sh"
+	"file-operations.sh"
 )
 
 # Global flags
@@ -21,54 +22,6 @@ https_enabled=true
 ACME_EAB_BLOCK=""
 CLOUDFLARE_ACME_BLOCK=""
 SECURITY_HEADERS_BLOCK=""
-
-########################################
-# Create a directory if it doesn't exist
-# Arguments:
-#   $1 - Directory path
-# Side Effects:
-#   Calls log_error and sets abort_config=true on failure
-########################################
-create_directory_if_not_exists() {
-	local dir="${1}"
-	if [[ ! -d "${dir}" ]]; then
-		mkdir -p "${dir}" || log_error "Unable to create directory '${dir}'."
-	fi
-}
-
-#######################################
-# Generic configuration file creator with template substitution
-# Arguments:
-#   $1 - Target config file path
-#   $2 - Description for logging
-#   $3 - File permissions (optional, defaults to 600)
-#######################################
-create_config_from_template() {
-    local config_path="${1}"
-    local description="${2}"
-    local permissions="${3:-600}"
-    local temp_config_path
-    
-    temp_config_path=$(mktemp) || {
-        log_error "Unable to create temporary file for ${description}"
-		return
-    }
-
-    if envsubst < "${config_path}" > "${temp_config_path}"; then
-        chmod "${permissions}" "${temp_config_path}"
-        if mv "${temp_config_path}" "${config_path}"; then
-            return
-        else
-            log_error "Unable to move ${description} to final location"
-            rm -f "${temp_config_path}"
-        fi
-    else
-        log_error "Unable to generate ${description}"
-        rm -f "${temp_config_path}"
-    fi
-
-	return
-}
 
 #######################################
 # Set default or validate PUBLIC_LISTEN_PORT
